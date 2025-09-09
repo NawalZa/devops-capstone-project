699b52f0899803affe328ae12b6f21514120fa18
NawalZa <nawal.zaher@dar.com>
2025-09-09 05:53:00 -0400

    update account added and tested

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,25 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        account = self._create_accounts(1)[0]
+        new_name = "Updated Name"
+        resp = self.client.put(
+            f"{BASE_URL}/{account.id}",
+            json={"name": new_name, "email": account.email, "address": account.address},
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], new_name)
+
+    def test_update_account_not_found(self):
+        """It should not Update an Account that does not exist"""
+        resp = self.client.put(
+            f"{BASE_URL}/0",
+            json={"name": "Ghost Account"},
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
